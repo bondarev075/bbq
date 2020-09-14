af889b486634a25b7622a03d4d3e1f00616d087b
bondarev075 <bondarev075@yandex.ru>
2020-09-14 19:23:07 +0700

    Fix CarrierWave credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws' # required
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
@@ -9,7 +10,7 @@ if Rails.env.production?
     }
 
     config.fog_directory = ENV['S3_BUCKET_NAME']
-    config.fog_public = false
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    # config.fog_public = false
+    # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 end
