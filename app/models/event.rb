8cc65ca6d37fea76249ee17e8739fb8638b8351a
bondarev075 <bondarev075@yandex.ru>
2020-09-17 19:53:51 +0700

    Fix event destroy error

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,11 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   validates :user, presence: true
 
-  validates :title, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
   validates :datetime, presence: true
 
