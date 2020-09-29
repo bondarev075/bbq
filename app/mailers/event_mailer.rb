855f9dcefaaaccd9ff938b62beeab6dfdba96051
bondarev075 <bondarev075@yandex.ru>
2020-09-29 22:07:50 +0700

    Fix error with photo url in email

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -19,7 +19,7 @@ class EventMailer < ApplicationMailer
     @photo = photo
     @event = event
 
-    path = photo.photo.thumb
+    path = photo.photo.thumb.url
     path = "#{Rails.root}/public#{path}" unless Rails.env.production?
     attachments.inline[photo.photo.file.filename] = File.read(path)
     mail to: email, subject: "Новая фотография в событии @ #{event.title}"
